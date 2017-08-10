d1eb87a7a3391f4460523f1d6d8838b5a8d670b9
airt <airtial@gmail.com>
2017-08-10 21:40:49 +0800

    feat(chapter6): update chapter6

---
+++ src/main/scala/learning/airt/concurrency/chapter6/RMap.scala
@@ -0,0 +1,24 @@
+package learning.airt.concurrency.chapter6
+
+import rx.lang.scala._
+
+import scala.collection.mutable
+
+class RMap[A, B] {
+
+  private val internal = mutable.Map[A, Subject[B]]()
+
+  def update(k: A, v: B): Unit = internal get k match {
+    case Some(ob) => ob onNext v
+    case None =>
+  }
+
+  def apply(k: A): Observable[B] = internal getOrElseUpdate(k, Subject())
+
+}
+
+object RMap {
+
+  def apply[A, B](): RMap[A, B] = new RMap
+
+}
---
+++ src/test/scala/learning/airt/concurrency/chapter6/RPriorityQueueSpec.scala
@@ -0,0 +1,32 @@
+package learning.airt.concurrency.chapter6
+
+import java.util.concurrent.atomic.AtomicInteger
+
+import org.scalatest._
+
+class RPriorityQueueSpec extends FreeSpec with Matchers {
+
+  "Exercises in Chapter 6" - {
+
+    "RPriorityQueue" - {
+
+      "apply and update" - {
+        "should work correctly" in {
+          val queue = RPriorityQueue[Int]()
+          val ob = queue.popped
+          val counter = new AtomicInteger(0)
+          ob subscribe { x => x shouldBe counter.incrementAndGet() }
+          queue add 2
+          queue add 1
+          queue pop()
+          queue pop()
+          Thread sleep 10
+          counter get() shouldBe 2
+        }
+      }
+
+    }
+
+  }
+
+}
